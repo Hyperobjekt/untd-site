20b4d6314cd886b8899d9ddf57180464f58b3a12
Lou Groshek <lou@lougroshek.com>
2021-01-22 17:08:10 -0600

    Adds menu toggle button

--- src/components/molecules/header/header.js
+++ src/components/molecules/header/header.js
@@ -1,60 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from 'reactstrap'
+import { Navbar, NavbarBrand, Button } from 'reactstrap'
+import { MdMenu } from 'react-icons/md'
 
 import './header.scss'
 
 const Header = ({ siteTitle, location }) => {
   const [isOpen, setIsOpen] = useState(false)
-  const toggle = () => setIsOpen(!isOpen)
-
-  const [fullLogo, setFullLogo] = useState(true)
-  const headerHeight = 89
-  const maxWidth = 768
-
-  const [isToggleMenu, setToggleMenu] = useState(false)
-
-  const logoSizeHandler = () => {
-    if (window.pageYOffset > headerHeight || window.innerWidth < maxWidth) {
-      // console.log('scrolled beyond header height')
-      setFullLogo(false)
-    } else {
-      setFullLogo(true)
-    }
-    // console.log('fullLogo', fullLogo)
-    if (window.innerWidth < maxWidth) {
-      setToggleMenu(true)
-    } else {
-      setToggleMenu(false)
-    }
-  }
-
-  useEffect(() => {
-    ;['scroll', 'resize', 'load'].forEach(function(e) {
-      window.addEventListener(e, logoSizeHandler)
-    })
-    logoSizeHandler()
-  })
 
   return (
-    <header
-      className={
-        (!!fullLogo ? 'full-logo' : '') +
-        ' ' +
-        (!!isToggleMenu ? 'toggle-menu' : '')
-      }
-    >
+    <header>
       <Navbar color="light" light expand="md" fixed="top">
         <NavbarBrand href="/">LOGO</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
+        <Button color="link" className="align-self-end">
+          <MdMenu />
+        </Button>
       </Navbar>
     </header>
   )
