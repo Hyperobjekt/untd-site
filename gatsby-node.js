4bf7b220035000459c08b4a2e458a5b174ea69b8
connor trotter <touchRED@users.noreply.github.com>
2021-06-14 14:14:23 -0500

    Feature/research lib revisions (#80)
    * research lib revisions
    
    * revert merge, small improvements
    
    * fix committed merge conflicts
    
    Co-authored-by: lane <laneolson@gmail.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,9 @@
+const slugify = string =>
+  string
+    .split(' ')
+    .map(ss => ss.toLowerCase())
+    .join('-')
+
 exports.createSchemaCustomization = ({ actions: { createTypes } }) => {
     createTypes(`
       type Mdx implements Node {
@@ -37,7 +43,13 @@ exports.createSchemaCustomization = ({ actions: { createTypes } }) => {
 
       type ResearchItem {
         item_content: String @mdx
+        item_content_sections: [ResearchItemSection]
         item_references: String @mdx
+      }
+
+      type ResearchItemSection {
+        section_title: String @mdx
+        section_content: String @mdx
       }
 
       type ContentRow {
@@ -49,3 +61,40 @@ exports.createSchemaCustomization = ({ actions: { createTypes } }) => {
       }
     `);
 };
+
+exports.createPages = async function ({ actions, graphql }) {
+  const { data } = await graphql(`
+    query {
+      allMdx(filter: { fileAbsolutePath: { regex: "/research-library/" } }) {
+        edges {
+          node {
+            frontmatter {
+              researchItems {
+                label
+                item_description
+                item_color
+                item_image
+                item_content
+                item_content_sections {
+                    section_title
+                    section_content
+                }
+                item_references
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  data.allMdx.edges[0].node.frontmatter.researchItems.forEach(node => {
+    actions.createPage({
+      path: `/research-library/${slugify(node.label)}/`,
+      component: require.resolve(`./src/templates/research-topic.js`),
+      context: {
+        ...node,
+        researchItems: data.allMdx.edges[0].node.frontmatter.researchItems.map(({label, item_image, item_description, item_color}) => ({label, item_image, item_description, item_color}))
+      },
+    })
+  })
+}
