37f1ca5bb84eabae5caee13c3df13dc70484860c
James Minton <james-minton@github.com>
2021-06-14 18:08:27 -0500

    Small duration increase

--- src/components/atoms/animation.js
+++ src/components/atoms/animation.js
@@ -18,7 +18,7 @@ export const basicStaggerChild = {
         opacity: 1,
         y: 0,
         transition: {
-            duration: 0.5,
+            duration: 0.6,
             delay: 0,
         }
     },
@@ -47,7 +47,7 @@ export const basicStaggerChildDown = {
         y: 0,
         transition: {
             delay: 0,
-            duration: 0.5,
+            duration: 0.6,
         }
     },
     hide: {
@@ -60,7 +60,7 @@ export const basicStaggerChildRight = {
         opacity: 1,
         x: 0,
         transition: {
-            duration: 0.1,
+            duration: 0.6,
             delay: 0,
         }
     },
