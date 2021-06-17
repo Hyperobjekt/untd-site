8c677282f12604cc2913a91b8759de2790e9ff93
Connor <connor@zero.nyc>
2021-06-17 14:51:33 -0400

    add @reach/skip-nav, include in layout and gatsby-browser

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 import 'typeface-lato'
 import 'typeface-muli'
+import { SkipNavLink, SkipNavContent } from "@reach/skip-nav"
+import "@reach/skip-nav/styles.css" //this will auto show and hide the link on focus
+
 
 import Header from './molecules/header'
 import Footer from './molecules/footer'
@@ -32,9 +35,11 @@ const Layout = ({ location, pageType, children }) => {
 
   return (
     <>
+      <SkipNavLink />
       {pageType !== 'explorer' && (
         <Header menu={data.site.siteMetadata.menu} siteTitle={data.site.siteMetadata.title} location={location} />
       )}
+      <SkipNavContent />
       <main className={`page-type-${pageType ? pageType : 'null'}`}>
         {children}
       </main>
